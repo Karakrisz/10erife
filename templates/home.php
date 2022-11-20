b15d48f675abb9b967144cb74f9e3ca03a3e3f4c
Boaz <webevolution13@gmail.com>
2022-11-21 00:42:14 +0100

    Front - end
    upDate

--- templates/home.php
+++ templates/home.php
@@ -59,7 +59,7 @@
                 </div>
             </div>
 
-            <div class="page-next-flex-box page-next-flex-box--img-url">
+            <div class="page-next-flex-box position-relative page-next-flex-box--img-url">
 
                 <div class="page-next-flex-box__img-box">
                     <img class="page-next-flex-box__img-box__img" src="Boaz/img/Layer14.png" alt="10erife">
@@ -80,17 +80,17 @@
             </div>
 
             <div class="page-flex-box page-flex-box--padding page-flex-box--two-img-url">
+
                 <div class="page-flex-box__div position-relative">
-                    <div class="page-flex-box__div__page-next-box position-absolute">
-                        <h2 class="page-flex-box__div__h2 page-flex-box__div__h2--color-c">The</h2>
-                        <h3 class="page-flex-box__div__h3">Space</h3>
-                        <div class="page-flex-box__div__next-link-box">
-                            <a class="one-page-link one-page-link--color-c" href="#">Gallery</a>
-                        </div>
-                    </div>
+                    <h2
+                        class="page-flex-box__div__h2 page-flex-box__div__h2--color-c position-absolute page-flex-box__div__h2--format">
+                        The</h2>
                 </div>
-                <div class="page-flex-box__div page-flex-box-next-text-box position-absolute">
-                    <p class="page-flex-box__div__p">Lorem Ipsum is simply dummy text of the printing and
+
+                <div class="page-flex-box__div position-relative">
+                    <p class="page-flex-box__div__p position-absolute page-flex-box__div__p--format">Lorem Ipsum is
+                        simply dummy text of
+                        the printing and
                         typesetting
                         industry. Lorem Ipsum
                         has
@@ -100,15 +100,37 @@
                         of
                         type
                         and scrambled it to make a type specimen book.</p>
-
-                    <p class="page-flex-box__div__p">It has survived not only five centuries, but also the leap into
-                        electronic typesetting, remaining
-                        essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
-                        containing Lorem Ipsum passages, and more recently with desktop publishing software like
-                        Aldus
-                        PageMaker including versions of Lorem Ipsum.</p>
                 </div>
+
             </div>
+
+            <div class="page-flex-box page-flex-box--padding page-flex-box--three-img-url">
+
+                <div class="page-flex-box__div position-relative">
+                    <h3 class="page-flex-box__div__h3 position-absolute page-flex-box__div__h3--format">Space</h3>
+                    <div
+                        class="page-flex-box__div__next-link-box position-absolute page-flex-box__div__next-link-box--format">
+                        <a class="one-page-link one-page-link--color-c" href="#">Gallery</a>
+                    </div>
+                </div>
+
+                <div class="page-flex-box__div position-relative">
+                    <p class="page-flex-box__div__p position-absolute page-flex-box__div__p--two-format">Lorem Ipsum is
+                        simply dummy text
+                        of the printing and
+                        typesetting
+                        industry. Lorem Ipsum
+                        has
+                        been
+                        the
+                        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                        of
+                        type
+                        and scrambled it to make a type specimen book.</p>
+                </div>
+
+            </div>
+
 
             <img class="parallax-item__one-img-box__img position-absolute" src="Boaz/img/paralax-box/Layer-29.png"
                 alt="10erife">
@@ -183,7 +205,9 @@
                 <div class="page-flex-box__div__page-box parallax-next-item__page-box position-absolute">
                     <h2 class="page-flex-box__div__h2">The</h2>
                     <h3 class="page-flex-box__div__h3 page-flex-box__div__h3--color-c">Area</h3>
-                    <a class="one-page-link" href="#">Map</a>
+                    <div class="page-flex-box__div__next-link-box page-flex-box__div__next-link-box--one-format">
+                        <a class="one-page-link" href="#">Map</a>
+                    </div>
                 </div>
             </div>
             <div class="page-flex-box__div">
@@ -214,7 +238,9 @@
                     <h2 class="page-flex-box__div__h2">To</h2>
                     <h3 class="page-flex-box__div__h3">Get</h3>
                     <h3 class="page-flex-box__div__h3">Around...</h3>
-                    <a class="one-page-link" href="#">titsa.com</a>
+                    <div class="page-flex-box__div__next-link-box page-flex-box__div__next-link-box--two-format">
+                        <a class="one-page-link" href="#">titsa.com</a>
+                    </div>
                 </div>
             </div>
             <div class="page-flex-box__div">
